d4143e4b9a7032e1dc2e2dc0630e868c7dc0b03a
nastya-zz <zelenenkaya9@gmail.com>
2021-04-30 22:37:21 +0300

    link modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
-import { ProductController } from './product/product.controller';
-import { ReviewController } from './review/review.controller';
-import { TopPageController } from './top-page/top-page.controller';
 import { AuthModule } from './auth/auth.module';
+import { ProductController } from './product/product.controller';
 import { ProductModule } from './product/product.module';
+import { ReviewController } from './review/review.controller';
 import { ReviewModule } from './review/review.module';
+import { TopPageController } from './top-page/top-page.controller';
 import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule],
-  controllers: [AppController, AuthController, ProductController, ReviewController, TopPageController],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    ProductModule,
+    TopPageModule,
+    ReviewModule
+  ],
+  controllers: [
+    AppController, 
+    AuthController, 
+    ProductController, 
+    ReviewController, 
+    TopPageController
+  ],
   providers: [AppService],
 })
 export class AppModule {}
