a311fafedf7cbeebc5a715967d208f2dbaff2ee5
nastya-zz <zelenenkaya9@gmail.com>
2021-04-30 22:35:45 +0300

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { AuthController } from './auth/auth.controller';
 import { ProductController } from './product/product.controller';
 import { ReviewController } from './review/review.controller';
 import { TopPageController } from './top-page/top-page.controller';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule],
   controllers: [AppController, AuthController, ProductController, ReviewController, TopPageController],
   providers: [AppService],
 })
