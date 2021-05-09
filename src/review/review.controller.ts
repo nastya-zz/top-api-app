18a68af3f41f63e35b99ca83b35f2fa854dc6cd9
nastya-zz <zelenenkaya9@gmail.com>
2021-05-09 18:59:28 +0300

    add review service

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,21 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { REVIEW_NOT_FOUND } from './review.constant';
 import { ReviewModel } from './review.model';
+import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
-    @Post('create')
-    async create(@Body() dto: Omit<ReviewModel, '_id'>) {
+	constructor(private readonly reviewService: ReviewService) { }
 
-    }
+	@Post('create')
+	async create(@Body() dto: CreateReviewDto) {
+		return this.reviewService.create(dto);
+	}
 
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		const deletedDoc = await this.reviewService.delete(id);
+		if (!deletedDoc) {
+			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+	}
 
-    }
-
-    @Get('byProduct/:productId')
-    async getByProducId(@Param('productId') productId: string) {
-
-    }
-
+	@Get('byProduct/:productId')
+	async getByProducId(@Param('productId') productId: string) {
+		return this.reviewService.findProductId(productId);
+	}
 }
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { ReviewController } from './review.controller';
 import { ReviewModel } from './review.model';
+import { ReviewService } from './review.service';
 
 @Module({
 	controllers: [ReviewController],
@@ -12,6 +13,7 @@ import { ReviewModel } from './review.model';
 				collection: 'Review'
 			}
 		}])
-	]
+	],
+	providers: [ReviewService]
 })
 export class ReviewModule { }
