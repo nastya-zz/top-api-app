282af18c514b60c4b232d23e409d2f8116cee57a
nastya-zz <zelenenkaya9@gmail.com>
2021-08-10 20:16:04 +0300

    add ability to convert images to webp format

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { FileElementResponse } from './dto/file-element.response';
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 
 @Controller('files')
 export class FilesController {
@@ -11,12 +12,21 @@ export class FilesController {
 		private readonly fileService: FilesService
 	) { }
 
-
 	@Post('upload')
 	@HttpCode(200)
 	@UseGuards(JwtAuthGuard)
 	@UseInterceptors(FileInterceptor('files'))
 	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
-		return this.fileService.saveFiles([file]);
+		const saveArray: MFile[] = [new MFile(file)];
+
+		if (file.mimetype.includes('image')) {
+			const buffer: Buffer = await this.fileService.convertToWebP(file.buffer);
+
+			saveArray.push(new MFile({
+				originalname: `${file.originalname.split('.')[0]}.webp`,
+				buffer
+			}));
+		}
+		return this.fileService.saveFiles(saveArray);
 	}
 }
